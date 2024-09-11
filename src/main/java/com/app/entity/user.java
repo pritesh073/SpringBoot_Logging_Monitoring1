d71ea627edadb17593c84b0bc41d6ad59ef5bc1f
pritesh073 <162885080+pritesh073@users.noreply.github.com>
2024-09-11 15:23:26 +0530

    First Commit

---
+++ src/main/java/com/app/entity/user.java
@@ -0,0 +1,23 @@
+package com.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+
+public class user {
+	
+	@Id
+	private String userId;
+	private String password;
+
+}
